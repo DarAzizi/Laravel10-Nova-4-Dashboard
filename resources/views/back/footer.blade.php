04152cb007fe6ae1f5183d1de9b8130d4614a551
Mounir Azizi <material.aziz@gmail.com>
2023-11-28 03:38:02 +0100

    Creating the 1st resource Product

---
+++ resources/views/back/footer.blade.php
@@ -0,0 +1,8 @@
+<div class="text-center">
+    <p>
+        Dashboard created and Powered by
+        <a href="https://azizimounir.com" class="text-yellow-600 hover:text-primary-500 transition-all font-bold">Azizi Mounir</a>
+        . Feel free to write whatever you want
+    </p>
+
+</div>
